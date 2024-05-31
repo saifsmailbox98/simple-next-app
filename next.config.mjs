f6a5b312740544ad6c76e8154b1f413c7d617bd0
Saif Ur Rahman <saifsmailbox98@users.noreply.github.com>
2024-05-31 23:14:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: `http://localhost:9090/${process.env.SESSION_BACKEND_STATIC_TOKEN}/`,
+  // assetPrefix: `http://localhost:9090/${process.env.SESSION_BACKEND_STATIC_TOKEN}/`,
   basePath: `/${process.env.SESSION_BACKEND_STATIC_TOKEN}`
 };
 
